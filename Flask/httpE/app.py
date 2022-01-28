4e9fd9fbf8dd4a54a279fd0636f3e6a31970c55a
13Igrok <47218346+13Igrok@users.noreply.github.com>
2022-01-28 19:00:48 +0300

    d

--- Flask/httpE/app.py
+++ Flask/httpE/app.py
@@ -1,7 +1,17 @@
-from app import routes
-from flask import Flask
+import json
+import os
+import re
+import ssl
+from dataclasses import fields
+from datetime import datetime
+from sqlite3 import apilevel
+from urllib.request import urlopen
+
+from flask import (Flask, Response, jsonify, make_response, render_template,
+                   request)
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'])
 def index(username):
@@ -25,20 +35,17 @@ def success():
     else:
         pass
 
+
 @app.route('/blogs')
 def blog():
     return render_template('blog.html')
+
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
 
-
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
-
-
-
-
