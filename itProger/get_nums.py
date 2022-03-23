724a4413c3b576179b9627beb48f726ac139f67e
igor.shuleshko@retailrocket.io <igor.shuleshko@retailrocket.io>
2022-03-23 09:25:22 +0300

    al

---
+++ itProger/get_nums.py
@@ -0,0 +1,15 @@
+import enum
+
+
+def get_nums(i):
+    enum = []
+    for n in l:
+        if n % 2 == 0:
+            enum.append(n)
+        if n > 90:
+            break
+    return enum
+
+
+nums = (10, 20 [25, 77, 82, 85, 90])
+print(get_nums(nums[2]))
