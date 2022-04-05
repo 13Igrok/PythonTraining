d4ac41b8a4ef68b17d659893583e890512220f6e
Igor Shuleshko <47218346+13Igrok@users.noreply.github.com>
2022-04-05 22:41:36 +0300

    ba

---
+++ old/banner.py
@@ -0,0 +1,48 @@
+banner = r"""
+                                                              ```.......`                
+                                                     `.-:/oosyyhmmdhhyyhdmy`              
+                                               `-:+shdmmNNNmdhyo/-...` `-sM-              
+                                          `.:ohmNMMMMNNdho:-``   `+s/     h`              
+                                       .:sdNMMMMMMNNy/-`         `-`     .o               
+                                   `.+hNMMMMMMNdsd+o`                   +s`               
+                                 .+ymNMMMMMNyo/:...   `..````          `+.                
+                               :ydmNMMMMNy:`      ..-/osshsyhso/.`-.   --                 
+            ommmhyyyso/-`   `+dddMMMMMh/`       `/ssmoyh.m:+m`omyh:-+ .:                  
+             oMMMMmhys/yMm/omhdMMMMNs.       `/yyddhMNMMNMmMmoN-.yd+`o:                   
+              -dMMMMMN+mdyNhhMMMmd+`     .-:oyddNMMMMMMMMMMMMMNyd:+s::                    
+               `hMMMMyNymhyNMNMN+`     `/+oyhsssdohydMdMMMMMMMMMNd//.                     
+                `mMMMmdmsdMMMoy.       /yhssysssyhdyds+h/MMMMMMms/`                       
+                 oMMMMdsNMMMMh:.       ``-..``   `-:oyhooo:NMMs.                          
+                 /MMMdhMMMdd-hs:                   `./ooyooso+                            
+                 sMMNmMMMMy:``+s-                     .oo+ho-                             
+                `mMMMMMMmdsy/` .//                      :y/h                              
+                /MMMMMMMy: .+o/`                        `o+-                              
+                dMMMMMMNsy/`  .`                ./. `.- `                                 
+               .MhNMMMM+ `-/:                 -o/``:-`                                    
+               oN:mMNMd                     `+/`.//`                                      
+               dh+mNhN:                    -+``sN/                                        
+              -NyymNmm                    -- +NMMy.                                       
+             .dMhdNNNh .                 -.-dMMMMNms.                                     
+            :mMMmmMMMh :                ``/hNMMMMMMMNy-                                   
+           oNMMMMmMMMd /                 +mhMMMMMMMMMMNs`                                 
+         .yMMMMMMMMMMN`o                /dyMMMMMMMMMMMMMd-                                
+        -mMMMMMMdNMMMM//-             `.ymdmmmNNNNMMMMMMMm.                               
+       /NMMMMMMdNyNMMMd`+            .`y+-+ooooosyhdmmmNNMy          `-/syhy:             
+      +NMMMMMMMNoNsdMMMs.:           :`N:          `.-/ohdM`      ./ymmmmmy-              
+     +MMMMMMMMMMyoMysNMMo--          :`M/               `/m-   `/ymNmhdds.                
+    /NmmNmmmmmmyyyNN/-hMMs-.         --yy                 .` -smMNdsymo-                  
+   -dmmmmmmmsshds/-`   :dMd:`        .s:m/                `:hMMMMhodh-                    
+  `dNNNNNNhyho-          :yNh-     `:ds:oNo.`           `+dMMMMN++Ny`                     
+  sMMMMMMmd/               `+hh/`   `:   +MNho+//+syo .sNMMMMNs.+M+                       
+ .MMMMMMm/                    `/ss+.      -dMMMMMMmdddMMMMMds+sNM/                        
+ sMMMMNo`                         .://:-`   :hNMMMMMMMMMMNmNMMMM+                         
+ mMMMy.                               ``.-..``.:sdMMMMMMMMMMMMMh                          
+-MMh:                                            `-omMMMMMMMMMN.                          
+/d/`                                                `/dddMMMMMo                           
+``                                                    `sdhMMMN.                           
+                                                        +ddNNy                            
+                                                         :hdm/                            
+                                                          /md`                            
+                                                           .-"""[1:]
+                                                           
+print(banner)
