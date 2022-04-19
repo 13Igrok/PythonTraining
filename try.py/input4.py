d361f575fa87d10d8879986ffd8275527823d3d9
Gi13rot <iextreme228@gmail.com>
2022-04-19 13:42:30 +0300

    sd

--- try.py/input4.py
+++ try.py/input4.py
@@ -1,6 +1,10 @@
-n=int(input("Число : "))
-for i in range(-n+1,n):
-    if((n-abs(i))%2)==1:
-        print(n-abs(i),end=" ")
-    else:
-        print("@ ",end=" ")
+def new_func():
+    n = int(input("Число : "))
+    for i in range(-n+1, n):
+        if((n-abs(i)) % 2) == 1:
+            print(n-abs(i), end=" ")
+        else:
+            print("@ ", end=" ")
+
+
+new_func()
