bf01790eeb4c02861ea52c90b8a9217e91ff6abc
Igor Shuleshko <47218346+13Igrok@users.noreply.github.com>
2023-01-14 14:31:12 +0300

    sd

--- Flask/httpE/app.py
+++ Flask/httpE/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
 
 
 @app.route('/')
