70b80a16d28cdd09fcc5692ec3cd97729c4cd0e1
Igor Shuleshko <47218346+13Igrok@users.noreply.github.com>
2023-01-24 19:06:36 +0300

    h

--- Flask/app.py
+++ Flask/app.py
@@ -1,44 +1,11 @@
-from flask import Flask, jsonify, send_from_directory
-
-app = Flask(__name__, static_folder='static/dist')
-
-
-@app.route('/')
-def index():
-    # тут просто пробрасываем файлик, без всякого препроцессинга
-    return app.send_static_file("index.html")
-
-@app.route('/dist/<path:path>')
-def static_dist(path):
-    # тут пробрасываем статику
-    return send_from_directory("static/dist", path)
+from pickle import APPEND
+from flask import Flask, render_template
 
 
-@app.route('/api/languages')
-def languages():
-    # а это простой метод, который будет возвращать список языков программирования
-    # который мы потом с помощью vue.js будем отображать
-    return jsonify({
-        'languages': [
-            'assembly',
-            'c#',
-            'c',
-            'c++',
-            'go',
-            'java',
-            'javascript',
-            'object c',
-            'pascal',
-            'perl',
-            'php',
-            'python',
-            'R',
-            'ruby',
-            'SQL',
-            'swift',
-            'visual basic',
-        ]
-    })
+def main():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+
+@APPEND.route('/about')
+def showSignUp():
+    return render_template('about.html')
