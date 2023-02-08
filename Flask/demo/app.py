96dad3a99011747208ac4d38ad047118920ee71d
Igor Shuleshko <47218346+13Igrok@users.noreply.github.com>
2023-02-08 14:51:50 +0300

    sql

--- Flask/demo/app.py
+++ Flask/demo/app.py
@@ -1,6 +1,7 @@
 import json
 import sqlite3
 
+import flask
 import pandas as pd
 import plotly
 import plotly.graph_objs as go
@@ -10,50 +11,9 @@ from flask_socketio import SocketIO, emit
 
 conn = sqlite3.connect('mydatabase.db')
 cursor = conn.cursor()
-global test_val
+from flask import render_template
+
 app = Flask(__name__)
-socketio = SocketIO(app)
-def create_plot(feature_importance):
-    feature_importance=feature_importance.reset_index(drop=True)
-    feature_importance = feature_importance.iloc[:5]
-    print(feature_importance)
-
-    data = [
-        go.Bar(
-            x=feature_importance['Age'], # assign x as the dataframe column 'x'
-            y=feature_importance['Name'], orientation='h'
-        )
-    ]
-
-    return json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-@socketio.on("response")
-def background_task_func():
-    global test_val
-    global plot
-    socketio.sleep(10)
-    data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}  
-    test_val= pd.DataFrame(data)
-    bar = create_plot(test_val)
-    plot=bar
-    if test_val.shape[0]>1:
-        print(test_val)
-        emit('response_output',plot ,broadcast=True)
-        socketio.sleep(1)
-        #return render_template('views/index_img_soc.html', plot=bar)
-    
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    global plot
-    executor.submit(background_task_func)
-    bar = create_plot(test_val)
-    
-    return render_template('views/index_img_soc.html', plot=bar)
-
-
-
-if __name__ == "__main__":
-    data ={'Name': [], 'Age': []}  
-    test_val= pd.DataFrame(data)   
-    executor = Executor(app)
-    socketio.run(app) 
+    return render_template('index.html')
