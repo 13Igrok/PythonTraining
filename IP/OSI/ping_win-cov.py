4b4de2d4c842ee37f280319b3207e83ab187f12a
Igor Shuleshko <47218346+13Igrok@users.noreply.github.com>
2023-03-16 13:58:00 +0300

    12

---
+++ IP/OSI/ping_win-cov.py
@@ -0,0 +1,13 @@
+import os
+
+with open("ip_list.txt") as file:
+    park = file.read().splitlines()
+    output = ""
+    for ip in park:
+        response = os.popen(f"ping -c 4 -n {ip} ").read()
+        if("Request timed out." in response or "unreachable" in response):
+            output += str(ip) + ' link is down'+'\n'
+        else:
+            output += str(ip) + ' is up '+'\n'
+    with open("ip_output.txt","w") as file:
+        file.write(output)
