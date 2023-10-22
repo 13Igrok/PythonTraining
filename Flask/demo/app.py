2262264f5d697033e77ee7c65908271a73e65576
13Igrok <47218346+13Igrok@users.noreply.github.com>
2023-10-22 11:48:34 +0300

    fd

--- Flask/demo/app.py
+++ Flask/demo/app.py
@@ -1,14 +1,14 @@
-import sqlite3
-
-from flask import Flask
-
-conn = sqlite3.connect ( 'mydb.db' )
-cursor = conn.cursor ()
-from flask import render_template
-
-app = Flask ( __name__ )
-
-
-@app.route ( '/' )
-def index():
-    return render_template ( 'index.html' )
+import sys
+from flask import Flask, render_template
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'content'
+POST_DIR = 'posts'
+ 
+app = Flask(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+app.config.from_object(__name__)
