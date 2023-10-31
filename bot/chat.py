0fbc6c02e43799ceaf50ce49f76be2aa081ce869
13Igrok <47218346+13Igrok@users.noreply.github.com>
2023-10-31 16:04:00 +0300

    f

---
+++ bot/chat.py
@@ -0,0 +1,20 @@
+import random
+
+# Задайте список приветственных фраз
+greetings = ["ПрКонечно, вот пример простого бота на Python, который отвечает на вашиивет!", "Здравствуйте!", "Приветствую!", сообщ "Добрый денияень!"]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+:
+
+while True:
+    user_input = input("```Выpython: ")
+    
+
+    if# "п Иривет" inм user_input.lower():
+       п bot_response = get_random_gорreeting()
+тируем библиотеку для работы с    else:
+        bot_response = "Извините, я не понимаю вас."
+
+    print("Бот:", bot_response)
