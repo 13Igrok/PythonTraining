a4cd47aff06f3b0372f931470d158cb644f65f05
13Igrok <47218346+13Igrok@users.noreply.github.com>
2023-11-06 17:38:48 +0300

    BD

--- BD/app.py
+++ BD/app.py
@@ -1,24 +1,33 @@
 import sqlite3
 
-from sqlite3 import Error
+# Создание подключения к базе данных
+conn = sqlite3.connect('database.db')
 
+# Создание курсора для выполнения SQL-запросов
+cursor = conn.cursor()
 
-def sql_connection():
-    try:
-        return sqlite3.connect ( 'mydb.db' )
-    except Error:
+# Создание таблицы пользователей, если она не существует
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT,
+                   password TEXT)''')
 
-        print ( Error )
+# Функция для регистрации нового пользователя
+def register_user(username, password):
+    # Проверка, что пользователь с таким именем не существует
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        print("Пользователь с таким именем уже существует.")
+        return
+    
+    # Вставка нового пользователя в таблицу
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    conn.commit()
+    print("Пользователь успешно зарегистрирован.")
 
+# Пример использования функции регистрации
+register_user("john123", "password123")
 
-def sql_table(con):
-    cursorObj = con.cursor ()
-
-    cursorObj.execute (
-        "CREATE TABLE employees(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)" )
-
-    con.commit ()
-
-
-con = sql_connection ()
-sql_table ( con )
+# Закрытие соединения с базой данных
+conn.close()
