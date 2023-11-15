aeeb31443f1888d3ad8d282a94d88b7e147f2835
13Igrok <47218346+13Igrok@users.noreply.github.com>
2023-11-15 19:52:02 +0300

    fdg

--- html/Flask/step1/mysite.py
+++ html/Flask/step1/mysite.py
@@ -1,4 +1,3 @@
-import sys
 from flask import Flask, render_template
 from flask_flatpages import FlatPages, pygments_style_defs
 from flask_frozen import Freezer
@@ -14,17 +13,20 @@ flatpages = FlatPages(app)
 freezer = Freezer(app)
 app.config.from_object(__name__)
 
+
 @app.route("/")
 def index():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
     posts.sort(key=lambda item: item['date'], reverse=True)
     return render_template('index.html', posts=posts, bigheader=True)
 
+
 @app.route('/posts/<name>/')
 def post(name):
     path = '{}/{}'.format(POST_DIR, name)
     post = flatpages.get_or_404(path)
     return render_template('post.html', post=post)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
