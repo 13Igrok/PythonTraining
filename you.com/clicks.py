3fa47e817241cd334cd489774b0731105aac6b36
13igrok <iextreme228@gmail.com>
2023-11-30 20:28:55 +0300

    0.001

---
+++ you.com/clicks.py
@@ -0,0 +1,21 @@
+import time
+from pynput.mouse import Button, Controller
+
+# Создаем экземпляр контроллера мыши
+mouse = Controller()
+
+# Координаты клика
+x = 100
+y = 200
+
+# Количество кликов
+clicks = 10
+
+# Задержка между кликами в секундах
+delay = 0.00001
+
+# Перемещаем курсор на указанные координаты и кликаем
+mouse.position = (x, y)
+for i in range(clicks):
+    mouse.click(Button.left)
+    time.sleep(delay)
